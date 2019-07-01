6a5c3ca0d25df25077e20b4b876b6f933d9a6132
cpiguave <cpiguave@CHD-QSISTEMS>
2019-07-01 17:07:24 -0500

    modificaciones pantalla calificador e inserción de datos a tabla score

--- qp_score/src/com/qmatic/calificador/calificaciones.java
+++ qp_score/src/com/qmatic/calificador/calificaciones.java
@@ -43,28 +43,37 @@ public class calificaciones extends HttpServlet {
 			con = conexionDB.getConnection();
 			// System.out.println("exito");
 			stmt = con.createStatement();
-			//para los GET
-			//rs = stmt.executeQuery("select * from dbo.calificaciones  where id = ");
 			
 
-			rs = stmt.executeQuery("select * from dbo.qp_encuesta  where modulo ='1' and estado = '0'");
-			if (rs.next()) {
-			    do {
-			    	
-			    	response.getWriter().append("0");
-			    } while(rs.next());
-			} else {
-				stmt.executeUpdate("INSERT INTO calificaciones " + "VALUES ('0','1','0')");
-				response.getWriter().append("1");
-			}
+			int fecha_creacion = 1072019;
+			int hora_creacion = 230;
+			int id_encuesta = 1;
+			int id_marca = 1;
+			String parametro = request.getParameter("ValCal");
+			
+			
+			stmt.executeUpdate("INSERT INTO [dbo].[qp_score]([id_marca],[id_encuesta],[fecha_creacion],[parametro],[hora_creacion]) VALUES('" + id_marca+ "','" + id_encuesta+ "','" + fecha_creacion+ "','" +parametro + "','" +  hora_creacion+"')");
+			response.sendRedirect("index.html");
+			//para los GET
+			//rs = stmt.executeQuery("select * from dbo.calificaciones  where id = ");
+			//rs = stmt.executeQuery("select * from dbo.qp_encuesta  where modulo ='1' and estado = '0'");
+			//if (rs.next()) {
+			 //   do {
+			 //   	response.getWriter().append("0");
+			 //   } while(rs.next());
+			//} else {
+				//stmt.executeUpdate("INSERT INTO qp_marca (nombre_marca,ruta_objeto_marca,id_tipo_marca,fecha_creacion,hora_creacion,estado) VALUES ('" + nombre_marca+ "','" + ruta_objeto_marca+ "','" + id_tipo_marca+ "','" + fecha_creacion+ "','" + hora_creacion+ "','" + estado+"')");
+				
+			//	response.getWriter().append("1");
+			//}
 			//para los POST
-			
-			
+
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
+			response.sendRedirect("error.html");
 			e.printStackTrace();
-			System.out.println("{Error ["+e.getErrorCode()+"]} : "+e.getMessage().toString());
+			//System.out.println("{Error ["+e.getErrorCode()+"]} : "+e.getMessage().toString());
 		}
 	//	response.getWriter().append("Served at: ").append(request.getContextPath());
 		
